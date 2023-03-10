664a9988dd02a764cf12b9bc8b608996ca119582
Shinya Kudoh <shinya_kd@yahoo.com>
2023-03-10 00:00:26 -0500

    commit tree dca7c0293daaa3a49cfa8901921cbeacdee79f65

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+// Code your solutions in this fi
+
+
+function writeCards(names){
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        //console.log(`Thank you ${names[i]} for a present`);
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+        //return `Thank you ${names[i]} for a present`
+    }
+    return messages;
+}
+
+function countDown(number) {
+
+while (number >= 0) {
+    console.log(number);
+    number--;
+    
+}
+}
+
